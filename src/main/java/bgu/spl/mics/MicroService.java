e85fba0f2ecea678d7c9e32f930adafd45f242ef
danbar06 <44751586+danbar06@users.noreply.github.com>
2018-11-29 10:11:28 +0200

    Update MicroService.java

--- src/main/java/bgu/spl/mics/MicroService.java
+++ src/main/java/bgu/spl/mics/MicroService.java
@@ -102,8 +102,7 @@ public abstract class MicroService implements Runnable {
      * 	       			null in case no micro-service has subscribed to {@code e.getClass()}.
      */
     protected final <T> Future<T> sendEvent(Event<T> e) {
-        Future<T> Fu=Bus.sendEvent(e);
-        return Fu;
+        return Bus.sendEvent(e);
     }
 
     /**
